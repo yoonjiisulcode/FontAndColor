6de10de95daa1297e3510cf10e0ca0a6a5f1d185
Yoonji Kim <yoonjii_code@naver.com>
2019-09-20 10:41:12 +0900

    폼 디자인

--- FontAndColor/Form1.Designer.cs
+++ FontAndColor/Form1.Designer.cs
@@ -28,20 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tlsMenu = new System.Windows.Forms.ToolStrip();
+            this.fontDIg = new System.Windows.Forms.FontDialog();
+            this.colorDIg = new System.Windows.Forms.ColorDialog();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 320);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(463, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(140, 17);
+            this.toolStripStatusLabel1.Text = "3205 김윤지 - 폰트 변경";
+            // 
+            // tlsMenu
+            // 
+            this.tlsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tlsMenu.Name = "tlsMenu";
+            this.tlsMenu.Size = new System.Drawing.Size(463, 25);
+            this.tlsMenu.TabIndex = 1;
+            this.tlsMenu.Text = "toolStrip1";
+            // 
+            // rtbText
+            // 
+            this.rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbText.Location = new System.Drawing.Point(0, 25);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(463, 295);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = resources.GetString("rtbText.Text");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 342);
+            this.ClientSize = new System.Drawing.Size(463, 342);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.tlsMenu);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "3205 김윤지";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStrip tlsMenu;
+        private System.Windows.Forms.FontDialog fontDIg;
+        private System.Windows.Forms.ColorDialog colorDIg;
+        private System.Windows.Forms.RichTextBox rtbText;
     }
 }
 
