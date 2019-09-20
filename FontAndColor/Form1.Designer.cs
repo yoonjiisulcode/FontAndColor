bbebfd93d676b552986f30c6c826b55b0555fbd4
Yoonji Kim <yoonjii_code@naver.com>
2019-09-20 10:50:43 +0900

    #2 폼 디자인 by 3205 김윤지

--- FontAndColor/Form1.Designer.cs
+++ FontAndColor/Form1.Designer.cs
@@ -35,7 +35,13 @@
             this.fontDIg = new System.Windows.Forms.FontDialog();
             this.colorDIg = new System.Windows.Forms.ColorDialog();
             this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.tsbtnFont = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbtnColor = new System.Windows.Forms.ToolStripButton();
             this.statusStrip1.SuspendLayout();
+            this.tsbtnFont.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -72,11 +78,51 @@
             this.rtbText.TabIndex = 2;
             this.rtbText.Text = resources.GetString("rtbText.Text");
             // 
+            // tsbtnFont
+            // 
+            this.tsbtnFont.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.tsbtnFont.Location = new System.Drawing.Point(0, 25);
+            this.tsbtnFont.Name = "tsbtnFont";
+            this.tsbtnFont.Size = new System.Drawing.Size(463, 25);
+            this.tsbtnFont.TabIndex = 3;
+            this.tsbtnFont.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnColor});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 50);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(463, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbtnColor
+            // 
+            this.tsbtnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnColor.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnColor.Image")));
+            this.tsbtnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnColor.Name = "tsbtnColor";
+            this.tsbtnColor.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnColor.Text = "toolStripButton2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(463, 342);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.tsbtnFont);
             this.Controls.Add(this.rtbText);
             this.Controls.Add(this.tlsMenu);
             this.Controls.Add(this.statusStrip1);
@@ -87,6 +133,10 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tsbtnFont.ResumeLayout(false);
+            this.tsbtnFont.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +150,10 @@
         private System.Windows.Forms.FontDialog fontDIg;
         private System.Windows.Forms.ColorDialog colorDIg;
         private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.ToolStrip tsbtnFont;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbtnColor;
     }
 }
 
